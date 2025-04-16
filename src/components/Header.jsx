1e51b30b6508041bf71ea7a59136d8f95ef78e95
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-16 22:42:16 +0200

    Use css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import logo from '../assets/logo.png';
 
-import './Header.css';
+import classes from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className={classes.header}>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
