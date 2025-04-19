a07fac4f88166727f119bac5554361177c00581f
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-19 16:03:29 +0200

    Implement typescript
    + use media queries
    + pseudo elements
    + custom styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,11 @@
-import { styled } from 'styled-components';
-
 import logo from '../assets/logo.png';
-
-// import './Header.css';
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mb-16 mt-8">
+    <header className="flex flex-col items-center md-8 md:mb-16 mt-8">
       <img src={logo} alt="A canvas" className="mb-8 w-44 h-44 object-contain"/>
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">ReactArt</h1>
-      <p className="paragraph">A community of artists and art-lovers.</p>
+      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">ReactArt</h1>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header >
   );
 }
